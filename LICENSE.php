a6a34a40b2d9db5db5d8a2b7c124556bdbb7cc1c
alexander <alexarcticart@gmail.com>
2018-09-21 11:53:19 +0300

    Commit with Notepad++

--- LICENSE.php
+++ LICENSE.php
@@ -6,3 +6,6 @@
 	some text
 	some text
 	some text
+	
+	C:\Program Files\Notepad++\notepad++.exe
+	git config --global core.editor "'C:\Program Files\Notepad++\notepad++.exe' -multiInst -notabbar -nosession noPlugin"
